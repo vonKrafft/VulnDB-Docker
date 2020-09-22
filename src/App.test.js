f64a0e9582d20e96f949bb6301fa5f663c03a3dc
vonKrafft <wandrille@vonkrafft.fr>
2020-09-22 20:59:37 +0200

    Updating test render to be compliant with react-router-dom

--- src/App.test.js
+++ src/App.test.js
@@ -14,10 +14,11 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = getByText(/VulnDB/i);
   expect(linkElement).toBeInTheDocument();
 });
